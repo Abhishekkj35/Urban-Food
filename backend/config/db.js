3153147a59ff9b0aa92ba3fb90b18a384af5b233
Abhishekkj35 <149960080+Abhishekkj35@users.noreply.github.com>
2024-09-22 09:59:20 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,4 @@
 
-import mongoose from "mongoose";
 
-export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://abhishekkj6810:abhi6810@cluster0.siiuclf.mongodb.net/food-delivery").then(() => console.log("DB Connected"))
-}
 
 
