6538cc922e90a842a03de6db7704714b81105f61
Abhishekkj35 <149960080+Abhishekkj35@users.noreply.github.com>
2025-01-23 13:51:42 +0530

    final

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url=import.meta.env.VITE_BACKEND_URL;
+  const url='https://urbanfood-backend.onrender.com';
   return (
     <div>
     <ToastContainer/>
